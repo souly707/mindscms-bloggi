b2b0a75ce909a8e935571d55b1c4f8f732561821
souly707 <sool.707@gmail.com>
2021-11-03 05:16:39 +0300

    project done

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,12 +80,6 @@
 
         <!-- Core plugin JavaScript-->
         <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-        <!-- Page level plugins -->
-        <script src="{{ asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
-
-        <!-- Page level custom scripts -->
-        <script src="{{ asset('backend/js/demo/chart-area-demo.js')}}"></script>
-        <script src="{{ asset('backend/js/demo/chart-pie-demo.js')}}"></script>
 
         <!-- Custom scripts for all pages-->
         <script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
